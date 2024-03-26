ca30dd890bc6d344ac882cf2a75be6ff1157e603
vao49 <fbrc.le.blanc@gmail.com>
2024-03-26 16:25:23 +0100

    Annulation du MERGE de Mihai

--- Mihai/js/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import { loadHTML } from "../../js/ajax_Html.js";
-
-// Programme PRINCIPAL
-window.addEventListener("load", () => {
-  loadHTML("id-header", "../HEADER_FOOTER/HEADER.html");
-  loadHTML("id-footer", "../HEADER_FOOTER/FOOTER.html");
-});
