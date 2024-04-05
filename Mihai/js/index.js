5bd5e3cf83bb3ed9dc31bac585bf8474d6e2487f
vao49 <fbrc.le.blanc@gmail.com>
2024-04-05 09:11:44 +0200

    Update index.js
    MIHAI j'ai changé le chemin vers HEADER et FOOTER

--- Mihai/js/index.js
+++ Mihai/js/index.js
@@ -4,6 +4,6 @@ import { loadHTML } from "../../js/ajax_Html.js";
 
 // Programme PRINCIPAL
 window.addEventListener("load", () => {
-  loadHTML("id-header", "../../HEADER_FOOTER/HEADER.html");
-  loadHTML("id-footer", "../../HEADER_FOOTER/FOOTER.html");
+  loadHTML("id-header", "../HEADER_FOOTER/HEADER.html");
+  loadHTML("id-footer", "../HEADER_FOOTER/FOOTER.html");
 });
